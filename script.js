d5bd366ce6a7a510e28bccd5ddf93e468c538c1d
anamads <102142087+anamads@users.noreply.github.com>
2024-03-15 19:58:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-/*var try = document.getElementsByClass('bold');
 
-// Add a mouseover event listener
-try.addEventListener('mouseover', () => {
-  // Change the button's background color
-  try.style.color = 'blue';
-});
-*/
 
 const press = document.getElementById("jsbut");
 
@@ -16,50 +9,7 @@ let pressButton = function () {
 
 press.addEventListener("mousedown", pressButton);
 
-/*
-let square = document.getElementsByClassName("square4");
 
-let hover = function () {
-  square.style.backgroundImage = "none";
-};
-
-square.addEventListener("mouseover", hover);*/
-
-/*
-const faqquestions = document.getElementsByClassName("questbox");
-const faqbutton = document.getElementById("faqbut");
-const faqanswers = document.getElementsByClassName("answers");
-
-let openFaq = function () {
-  faqanswers.style.display = "block";
-};
-
-faqquestions.addEventListener("click", openFaq);
-faqbutton.addEventListener("click", openFaq);
-
-var faqquestions = document.getElementsByClassName("questbox");
-var faqbutton = document.getElementById("faqbut");
-var faqanswers = document.getElementsByClassName("answers");
-
-for (var i = 0; i < faqquestions.length; i++) {
-  faqquestions[i].onclick = function () {
-    var setClasses = !this.classList.contains("active");
-    setClass(faqquestions, "active", "remove");
-    setClass(answers, "show", "remove");
-
-    if (setClasses) {
-      this.classList.toggle("active");
-      this.nextElementSibling.classList.toggle("show");
-    }
-  };
-}
-
-function setClass(els, className, fnName) {
-  for (var i = 0; i < els.length; i++) {
-    els[i].classList[fnName](className);
-  }
-}
-*/
 const faqs = document.querySelectorAll(".faq");
 for (const item of faqs) {
   const curr_faq = item.childNodes;
