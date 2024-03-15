21f3257e93d7bb801ee2e95d5235d80b50dca5bb
anamads <102142087+anamads@users.noreply.github.com>
2024-03-15 20:51:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,27 +10,36 @@ let pressButton = function () {
 press.addEventListener("mousedown", pressButton);
 
 
-const faqs = document.querySelectorAll('.faq');
-for (const item of faqs) {
-  const curr_faq = item.childNodes;
-  console.log(curr_faq);
-  const question = curr_faq[0];
-  const answer = curr_faq[1];
-  const icon = question.querySelector(".icon-main");
-  icon.addEventListener("click", function () {
-    answer.classList.toggle("non-active");
-    const i = icon.querySelector("faqbut");
-    i.classList.toggle("fa-xmark");
-    i.classList.toggle("fa-plus");
-  });
-}
+const hamb = document.getElementById("hamburger-icon");
+const burgerlong = document.querySelector(".hamburger");
 
-const hamb = document.tophead.getElementById("hamburger-icon");
-const burgerlong = document.tophead.querySelector(".hamburger");
-
-let pressham = function () {
+let pressham = function (e) {
+  console.log(e.target);
   hamb.style.display = "none";
   burgerlong.style.display = "flex";
   burgerlong.style.flexDirection = "column";
 };
 hamb.addEventListener("click", pressham);
+
+const faqs = document.querySelectorAll(".faq");
+for (const item of faqs) {
+  const curr_faq = item.childNodes;
+  console.log(curr_faq);
+  const question = curr_faq[1];
+  const answer = curr_faq[3];
+  const icon = question.querySelector(".icon-main");
+  icon.addEventListener("click", function () {
+    answer.classList.toggle("non-active");
+    const i = icon.querySelector("i");
+    i.classList.toggle("fa-xmark");
+    i.classList.toggle("fa-plus");
+  });
+}
+
+const close = document.getElementById("closeburger");
+const closeFunction = (e) => {
+  hamb.style.display = "block";
+  burgerlong.style = "none";
+};
+close.addEventListener("click", closeFunction);
+
